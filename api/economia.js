33346e0327baec0517c1c70225ba5ba2f73f67dd
hallissonfr <hallissonhr@gmail.com>
2025-07-02 15:59:56 -0300

    Add files via upload

--- api/economia.js
+++ api/economia.js
@@ -1,21 +1,27 @@
-import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).send("Método não permitido");
   }
 
+  let browser = null;
+
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
     });
 
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+
     const response = await page.goto("https://agenciabrasil.ebc.com.br/rss/economia/feed.xml", {
       waitUntil: "networkidle0",
-      timeout: 15000
+      timeout: 15000,
     });
 
     const xml = await response.text();
@@ -24,6 +30,7 @@ export default async function handler(req, res) {
     res.setHeader("Content-Type", "application/rss+xml");
     res.status(200).send(xml);
   } catch (e) {
+    if (browser) await browser.close();
     res.status(500).send("Erro ao buscar RSS: " + e.message);
   }
 }
